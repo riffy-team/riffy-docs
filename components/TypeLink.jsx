6d73f1df169e84ed702e6437d9b4df20dfaea16c
UnschooledGamer <unschooledgamer@gmail.com>
2023-11-02 14:38:40 +0000

    remove console.log

--- components/TypeLink.jsx
+++ components/TypeLink.jsx
@@ -3,8 +3,7 @@ import Link from 'next/link'
  * @description Used to add links to Types(Number, string etc.) 
  * @param {import('next/link').LinkProps} linkProps link Properties 
 */
-export default function TypeLink({ style = { color: '#a8aff8'}, ...props }, ...a) {
-    console.log("props", style, props, a)
+export default function TypeLink({ style = { color: '#a8aff8'}, ...props }) {
     return (
         <Link style={style} {...props}>{props.label}</Link>
     )
