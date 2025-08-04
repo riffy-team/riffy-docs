2586d4e3e9cfb6fe989b42715d4fe40052391136
UnschooledGamer <unschooledgamer@gmail.com>
2025-08-04 19:54:19 +0530

    feat(plugins-remote-mdx-content): remote MDX content cached using Next.js's unstable_cache
    To prevent re-fetching & re-compiling.

--- app/plugins/[[...slug]]/page.tsx
+++ app/plugins/[[...slug]]/page.tsx
@@ -4,10 +4,9 @@ import { Callout, Tabs } from 'nextra/components'
 import { evaluate } from 'nextra/evaluate'
 import {
 	convertToPageMap,
-	mergeMetaWithPageMap,
-	normalizePageMap
 } from 'nextra/page-map'
 import { useMDXComponents as getMDXComponents } from '../../../mdx-components'
+import { unstable_cache } from 'next/cache'
 
 const remotes = {
 	'riffy-spotify': {
@@ -36,19 +35,12 @@ function updateRoutes(items: any, parentRoute: any) {
 		const parentRouteSegments = parentRoute.split('/').filter(Boolean);
 		const lastParentSegment = parentRouteSegments[parentRouteSegments.length - 1];
 
-		console.dir({ currentItemRoute, parentRouteSegments, lastParentSegment })
 		if (lastParentSegment && currentItemRoute.startsWith(`/${lastParentSegment}`)) {
-
-			console.log("currentItemRoute & lastParentSegement same check", currentItemRoute, lastParentSegment);
 			currentItemRoute = currentItemRoute.substring(lastParentSegment.length + 1);
-			console.log("[final - result ] currentItemRoute & lastParentSegement same check", currentItemRoute, lastParentSegment);
 		}
 
 		if (currentItemRoute.startsWith('/')) {
-			console.log("startWith check", currentItemRoute);
 			currentItemRoute = currentItemRoute.substring(1);
-
-			console.log("startWith check result", currentItemRoute);
 		}
 		const newRoute = `${parentRoute}/${currentItemRoute}`.replace(/\/\/+/g, '/');
 
@@ -57,7 +49,7 @@ function updateRoutes(items: any, parentRoute: any) {
 			route: newRoute,
 			title: item.title || item.name.replace(/\.mdx?$/, '').replace(/-/g, ' ').replace(/_/g, ' ').replace(/\b\w/g, (l: any) => l.toUpperCase())
 		};
-		console.log(updatedItem)
+
 		if (item.children) {
 			updatedItem.children = updateRoutes(item.children, newRoute);
 			updatedItem.type = 'menu';
@@ -82,10 +74,8 @@ for (const repo in remotes) {
 		const rootPluginItem = _pageMap[0];
 		const pluginRootRoute = `/plugins/${repo}`;
 
-		console.log(_pageMap)
 		//@ts-ignore
 		const isReadmeIndex = _pageMap.length === 1 && (rootPluginItem?.children?.length === 1 && rootPluginItem.children[0].name === 'README');
-		console.log(isReadmeIndex, rootPluginItem)
 
 		const finalPluginItem = {
 			...rootPluginItem,
@@ -104,7 +94,6 @@ for (const repo in remotes) {
 			delete finalPluginItem.children
 		}
 
-		console.log(finalPluginItem)
 		pageMapItems.push(finalPluginItem);
 	}
 }
@@ -123,36 +112,48 @@ type PageProps = Readonly<{
 	}>
 }>
 
+const getPage = unstable_cache(async (slug: string[]) => {
+    const route = slug?.join('/') ?? ''
+    //@ts-ignore
+    const [repoName, ...filePathParts] = slug
+    const finalRoute = filePathParts.length === 0 ? `${route}/README` : route
+    const filePath = mdxPages[finalRoute as keyof typeof mdxPages]
+
+    if (!filePath) {
+        return null
+    }
+
+    const remote = remotes[repoName as keyof typeof remotes]
+    if (!remote) {
+        return null
+    }
+
+    const { user, repo, branch, docsPath } = remote
+
+    const response = await fetch(
+        `https://raw.githubusercontent.com/${user}/${repo}/${branch}/${docsPath ? docsPath + '/' : ''
+        }${filePath}`
+    )
+
+    if (!response.ok) {
+        return null
+    }
+
+    const data = await response.text()
+    const rawJs = await compileMdx(data, { filePath })
+    return evaluate(rawJs, components)
+}, ['remote-mdx-pages'])
+
 export default async function Page(props: PageProps) {
 	const params = await props.params
-	const route = params.slug?.join('/') ?? ''
-	//@ts-ignore
-	const [repoName, ...filePathParts] = params.slug
-	const filePathName = filePathParts.join('/') || 'index'
-	const finalRoute = filePathParts.length === 0 ? `${route}/README` : route
-	const filePath = mdxPages[finalRoute as keyof typeof mdxPages]
+    const slug = params.slug || []
+	const page = await getPage(slug)
 
-
-	console.log({ filePathParts, filePath, filePathName, route, repoName, mdxPages })
-	if (!filePath) {
+	if (!page) {
 		notFound()
 	}
 
-	const remote = remotes[repoName as keyof typeof remotes]
-	if (!remote) {
-		notFound()
-	}
-
-	const { user, repo, branch, docsPath } = remote
-
-	const response = await fetch(
-		`https://raw.githubusercontent.com/${user}/${repo}/${branch}/${docsPath ? docsPath + '/' : ''
-		}${filePath}`
-	)
-	console.log(response.status)
-	const data = await response.text()
-	const rawJs = await compileMdx(data, { filePath })
-	const { default: MDXContent, toc, metadata } = evaluate(rawJs, components)
+    const { default: MDXContent, toc, metadata } = page
 
 	return (
 		<Wrapper toc={toc} metadata={metadata}>
@@ -162,9 +163,7 @@ export default async function Page(props: PageProps) {
 }
 
 export function generateStaticParams() {
-	const params = Object.keys(mdxPages).map(route => ({
+	return Object.keys(mdxPages).map(route => ({
 		slug: route.split('/')
 	}))
-
-	return params
 }
